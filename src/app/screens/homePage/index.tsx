cb6703a7ab18992023cd653cfbef737aa1a4d155
boburishmetov <boburmiro-77@mail.ru>
2024-10-10 22:53:49 +0900

    fix: modify statistics sectional component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
+import "../../../css/home.css";
 
 export default function HomePage() {
   return (
